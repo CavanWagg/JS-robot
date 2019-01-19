ef0929fb82c53fbad2a289a6862bd14d58d50047
Cavan Wagg <waggx002@gmail.com>
2019-01-19 14:22:21 -0600

    randomPick and randomRobot

--- main.js
+++ main.js
@@ -39,15 +39,15 @@ const roadGraph = buildGraph(roads);
 // a new state for the situation after the move.
 
 class VillageState {
-  constructor(place, parcels) {
-    this.place = place;
+  constructor(currentPlace, parcels) {
+    this.currentPlace = currentPlace;
     this.parcels = parcels;
   }
 
   move(destination) {
     // check whether there is a road from current place to destination
     // if not, you cannot move so return old state
-    if (!roadGraph[this.place].includes(destination)) {
+    if (!roadGraph[this.currentPlace].includes(destination)) {
       return this;
     }
     // road exists, create new state with the destination as the robot's new place.
@@ -57,32 +57,66 @@ class VillageState {
       let parcels = this.parcels
         // map takes care of the moving
         .map(p => {
-          if (p.place != this.place) return p;
-          console.log('map', { place: destination, address: p.address });
-          return { place: destination, address: p.address };
+          if (p.currentPlace != this.currentPlace) return p;
+          // console.log('map', { currentPlace: destination, dropOffAddress: p.dropOffAddress });
+          return {
+            currentPlace: destination,
+            dropOffAddress: p.dropOffAddress
+          };
         })
-        // filter does the delivering, if place = address, remove the parcel
-        .filter(p => p.place != p.address);
-      console.log('filter', new VillageState(destination, parcels));
+        // filter does the delivering, if currentPlace = dropOffAddress, remove the parcel
+        .filter(p => p.currentPlace != p.dropOffAddress);
+      // console.log('filter', new VillageState(destination, parcels));
       return new VillageState(destination, parcels);
     }
   }
 }
 
-let first = new VillageState('Post Office', [
-  { place: 'Post Office', address: "Alice's House" }
-]);
-let next = first.move("Alice's House");
+// let first = new VillageState('Post Office', [
+//   { currentPlace: 'Post Office', dropOffAddress: "Alice's House" }
+// ]);
+// let next = first.move("Alice's House");
 
-// console.log(next.place);
+// console.log(next.currentPlace);
 // console.log(next.parcels);
-// console.log(first.place);
+// console.log(first.currentPlace);
 
-// function runRobot(state, robot, memory) {
-//   for (let turn = 0;; turn++) {
-//     if (state.parcels.length == 0) {
-//       console.log(`Done in ${turn} turns`);
-//       break
-//     }
-//   }
-// }
+function runRobot(state, robot, memory) {
+  for (let turn = 0; ; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+
+// dumb solution, robot walks in random direction
+function randomPick(arr) {
+  let choice = Math.floor(Math.random() * arr.length);
+  return arr[choice];
+}
+
+function randomRobot(state) {
+  return { direction: randomPick(roadGraph[state.currentPlace]) };
+}
+
+VillageState.random = function(parcelCount = 5) {
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++) {
+    let dropOffAddress = randomPick(Object.keys(roadGraph));
+    let currentPlace;
+    // We don't want any parcels sent from the same place they are addressed to
+    // the do loop keeps picking new places when it gets one that's equal to the address
+    do {
+      currentPlace = randomPick(Object.keys(roadGraph));
+    } while (currentPlace == dropOffAddress);
+    parcels.push({ currentPlace, dropOffAddress });
+  }
+  return new VillageState('Post Office', parcels);
+};
+
+// runRobot(VillageState.random(), randomRobot);
